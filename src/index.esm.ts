3719ee9d27d725bb36cf018969546d70ef0a5972
Radek Šerý <radek.sery@peckadesign.cz>
2023-12-06 12:23:43 +0100

    fix: Fixed exports of new features

--- src/index.esm.ts
+++ src/index.esm.ts
@@ -3,13 +3,15 @@ import ControlManager from './utils/ControlManager'
 export { AjaxModalExtension } from './extensions/AjaxModalExtension'
 export { AjaxModalPreventRedrawExtension } from './extensions/AjaxModalPreventRedrawExtension'
 export { AjaxOnceExtension } from './extensions/AjaxOnceExtension'
+export { BtnSpinnerExtension } from './extensions/BtnSpinnerExtension'
 export { ConfirmExtension } from './extensions/ConfirmExtension'
 export { FollowUpRequestExtension } from './extensions/FollowUpRequestExtension'
 export { ForceRedirectExtension } from './extensions/ForceRedirectExtension'
 export { ForceReplaceExtension } from './extensions/ForceReplaceExtension'
+export { SingleSubmitExtension } from './extensions/SingleSubmitExtension'
 export { SnippetFormPartExtension } from './extensions/SnippetFormPartExtension'
 export { SpinnerExtension } from './extensions/SpinnerExtension'
 
-export { isDatasetTruthy } from './utils'
+export { isDatasetTruthy, isDatasetFalsy } from './utils'
 
 export const controlManager = new ControlManager()
